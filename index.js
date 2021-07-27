a0ca9faf311656308bc815b10bfe1b8243a14f57
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-07-27 14:51:56 -0400

    adding updated questions

--- index.js
+++ index.js
@@ -1,62 +1,96 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: "What is your GitHub username?",
+    name: 'username',
+    default: 'TroyBohrer8',
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a valid GitHub username.");
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    message: "What is your GitHub repo name?",
+    name: 'repo',
+    default: 'Great-ReadMe-Generator',
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a valid Github repo name.");
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    message: "What is the title of this project?",
+    name: 'project-title',
+    default: 'My Exciting Project',
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a valid project name.");
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    message: "Write a description of what your project entails.",
+    name: 'description',
+    default: 'This project solves all of the worlds problems!',
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("Please enter a valid project description.");
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    message: "Please describe the steps to properly install your project for the Insallation section.",
+    name: 'installation'
+  },
+  {
+    type: 'input',
+    message: "Please provide instructions and an example of your project in use for the Usage section.",
+    name: 'usage'
+  },
+  {
+    type: 'input',
+    message: "Please provide steps on how other developers can contribute to your project.",
+    name: 'contribution'
+  },
+  {
+    type: 'input',
+    message: "Please provide any tests you have created for your project.",
+    name: 'tests'
+  },
+  {
+    type: 'list',
+    message: "Please choose a license for your project",
+    choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+    name: 'license'
+  },
+];
 
-const writeFileAsync = util.promisify(fs.writeFile);
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Success! Your README.md file has been created.")
+  });
+}
 
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
-    },
-    {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
-    },
-  ]);
-};
+ const writeFileAsync = util.promisify(writeToFile);
 
 
-// Bonus using writeFileAsync as a promise
-const init = () => {
-  promptUser()
-    .then((answers) => writeFileAsync('README.md', generateHTML(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
-    .catch((err) => console.error(err));
-};
 
 init();
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
